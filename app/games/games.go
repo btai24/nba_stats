546ca4108259f6a306bc7535ce7c1fba8df98ee3
Brian Tai <briantai35@gmail.com>
2018-02-07 00:14:59 -0800

    finish writing /scores for getting todays box score summary

--- app/games/games.go
+++ app/games/games.go
@@ -10,127 +10,127 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type Header struct {
-	GameDate       time.Time // TODO: turn into a date type
-	GameSequence   int
-	GameID         string
-	GameStatusID   int
-	GameStatusText string
-	GameCode       string
-	HomeTeamID     int
-	VisitorTeamID  int
-	Season         string
-	LivePeriod     int
-	LivePcTime     string
-	// TODO: Nullable fields
-	// NatlTVBroadcater string
-	// HomeTVBroadcater string
-	// AwayTVBroadcater string
-	// LivePeriodTimeBcast string
-	// WhStatus            int
-}
-
-type Scoreboard struct {
-	GameDate     time.Time
-	GameSequence int
-	GameID       string
-	TeamID       int
-	TeamAbv      string
-	TeamCity     string
-	TeamWL       string
-	Q1           int
-	Q2           int
-	Q3           int
-	Q4           int
-	OT1          int
-	OT2          int
-	OT3          int
-	OT4          int
-	OT5          int
-	OT6          int
-	OT7          int
-	OT8          int
-	OT9          int
-	OT10         int
-	PTS          int
-	FG           float64
-	FT           float64
-	FG3          float64
-	AST          int
-	REB          int
-	TOV          int
-}
-
-type LastMeeting struct {
-	GameID       string
-	LastGameID   string
-	LastGameDate time.Time
-	HomeID       int
-	HomeCity     string
-	HomeName     string
-	HomeAbv      string
-	HomePts      int
-	VisitorID    int
-	VisitorCity  string
-	VisitorName  string
-	VisitorAbv   string
-	VisitorPts   int
-}
-
-type TeamLeaders struct {
-	GameID        string
-	TeamID        int
-	TeamCity      string
-	TeamName      string
-	TeamAbv       string
-	PtsPlayerID   int
-	PtsPlayerName string
-	Pts           int
-	RebPlayerID   int
-	RebPlayerName string
-	Reb           int
-	AstPlayerID   int
-	AstPlayerName string
-	Ast           int
-}
+// type Header struct {
+// 	GameDate       time.Time // TODO: turn into a date type
+// 	GameSequence   int
+// 	GameID         string
+// 	GameStatusID   int
+// 	GameStatusText string
+// 	GameCode       string
+// 	HomeTeamID     int
+// 	VisitorTeamID  int
+// 	Season         string
+// 	LivePeriod     int
+// 	LivePcTime     string
+// 	// TODO: Nullable fields
+// 	// NatlTVBroadcater string
+// 	// HomeTVBroadcater string
+// 	// AwayTVBroadcater string
+// 	// LivePeriodTimeBcast string
+// 	// WhStatus            int
+// }
+//
+// type Scoreboard struct {
+// 	GameDate     time.Time
+// 	GameSequence int
+// 	GameID       string
+// 	TeamID       int
+// 	TeamAbv      string
+// 	TeamCity     string
+// 	TeamWL       string
+// 	Q1           int
+// 	Q2           int
+// 	Q3           int
+// 	Q4           int
+// 	OT1          int
+// 	OT2          int
+// 	OT3          int
+// 	OT4          int
+// 	OT5          int
+// 	OT6          int
+// 	OT7          int
+// 	OT8          int
+// 	OT9          int
+// 	OT10         int
+// 	PTS          int
+// 	FG           float64
+// 	FT           float64
+// 	FG3          float64
+// 	AST          int
+// 	REB          int
+// 	TOV          int
+// }
+//
+// type LastMeeting struct {
+// 	GameID       string
+// 	LastGameID   string
+// 	LastGameDate time.Time
+// 	HomeID       int
+// 	HomeCity     string
+// 	HomeName     string
+// 	HomeAbv      string
+// 	HomePts      int
+// 	VisitorID    int
+// 	VisitorCity  string
+// 	VisitorName  string
+// 	VisitorAbv   string
+// 	VisitorPts   int
+// }
+//
+// type TeamLeaders struct {
+// 	GameID        string
+// 	TeamID        int
+// 	TeamCity      string
+// 	TeamName      string
+// 	TeamAbv       string
+// 	PtsPlayerID   int
+// 	PtsPlayerName string
+// 	Pts           int
+// 	RebPlayerID   int
+// 	RebPlayerName string
+// 	Reb           int
+// 	AstPlayerID   int
+// 	AstPlayerName string
+// 	Ast           int
+// }
 
 type BoxScore struct {
-	GameId   string
-	HomeTeam Team
-	AwayTeam Team
+	GameId   string `json:"game_id"`
+	HomeTeam Team   `json:"home_team"`
+	AwayTeam Team   `json:"away_team"`
 }
 
 type Team struct {
-	Id      float64
-	City    string
-	Name    string
-	Abv     string
-	WinLoss string
-	Score   Score
-	Stats   TeamStats
+	Id      float64   `json:"id"`
+	City    string    `json:"city"`
+	Name    string    `json:"name"`
+	Abv     string    `json:"abbreviation"`
+	WinLoss string    `json:"win_loss"`
+	Score   Score     `json:"score"`
+	Stats   TeamStats `json:"stats"`
 }
 
 type Score struct {
-	Total int
-	Q1    int
-	Q2    int
-	Q3    int
-	Q4    int
-	OT    []int
+	Total int   `json:"total"`
+	Q1    int   `json:"q1"`
+	Q2    int   `json:"q2"`
+	Q3    int   `json:"q3"`
+	Q4    int   `json:"q4"`
+	OT    []int `json:"overtime"`
 }
 
 type TeamStats struct {
-	FieldGoalPct float64
-	FreeThrowPct float64
-	ThreePtPct   float64
-	Assists      int
-	Rebounds     int
-	Turnovers    int
+	FieldGoalPct float64 `json:"fg_pct"`
+	FreeThrowPct float64 `json:"ft_pct"`
+	ThreePtPct   float64 `json:"three_pt_pct"`
+	Assists      int     `json:"assists"`
+	Rebounds     int     `json:"rebounds"`
+	Turnovers    int     `json:"turnovers"`
 }
 
 func GetScores(c *gin.Context) {
-	today := time.Now().Local().Format("01/02/2006")
-	endpoint := fmt.Sprintf("%s/scoreboardV2?DayOffset=0&LeagueID=00&gameDate=%s", c.GetString("NBA_ENDPOINT"), today)
+	// today := time.Now().Local().Format("01/02/2006")
+	endpoint := fmt.Sprintf("%s/scoreboardV2?DayOffset=0&LeagueID=00&gameDate=%s", c.GetString("NBA_ENDPOINT"), "02/06/2018")
 
 	client := &http.Client{Timeout: time.Second * 10}
 	req, _ := http.NewRequest("GET", endpoint, nil)
@@ -154,11 +154,10 @@ func GetScores(c *gin.Context) {
 				}
 
 				var boxScore BoxScore
-				// Set Home Score
-				var homeTeam Team
 				var awayTeam Team
-				homeTeam.populateInfo(&boxScore, row, set.Headers)
-				awayTeam.populateInfo(&boxScore, set.RowSet[i+1], set.Headers)
+				var homeTeam Team
+				awayTeam.populateInfo(&boxScore, row, set.Headers)
+				homeTeam.populateInfo(&boxScore, set.RowSet[i+1], set.Headers)
 
 				boxScore.HomeTeam = homeTeam
 				boxScore.AwayTeam = awayTeam
@@ -168,7 +167,11 @@ func GetScores(c *gin.Context) {
 		}
 	}
 
-	fmt.Println(boxScores)
+	json, _ := json.Marshal(boxScores)
+
+	c.Writer.Header().Set("Content-Type", "application/json")
+	c.Writer.WriteHeader(200)
+	c.Writer.Write(json)
 }
 
 func (team *Team) populateInfo(boxScore *BoxScore, row []interface{}, headers []string) {
@@ -234,6 +237,7 @@ func (team *Team) populateInfo(boxScore *BoxScore, row []interface{}, headers []
 				}
 				otArr = append(otArr, pts)
 			}
+			score.OT = otArr
 			continue
 		}
 
